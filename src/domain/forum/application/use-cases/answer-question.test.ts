07ec42921f3409c9dbf58890d30cda366661ae91
Deivison Resende <deivison4844@gmail.com>
2023-11-29 22:29:44 -0300

    feat:Adds existing question validation to create an answer

--- src/domain/forum/application/use-cases/answer-question.test.ts
+++ src/domain/forum/application/use-cases/answer-question.test.ts
@@ -1,21 +1,46 @@
-import { Answer } from '../../enterprise/entities/answer'
-import { AnswersRepository } from '../repositories/answers-repository'
-import { AnswerQuestionUseCase as SUT } from './answer-question'
+import { AnswerQuestionUseCase } from './answer-question'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { faker } from '@faker-js/faker'
+import { makeQuestion } from 'test/factories/make-question'
 
-const InMemoryAnswerRepository: AnswersRepository = {
-  create: async (answer: Answer): Promise<Answer> => {
-    return answer
-  }
-}
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let SUT: AnswerQuestionUseCase
 
-test('create an answer', async () => {
-  const answerQuestion = new SUT(InMemoryAnswerRepository)
+describe('Answer question use case', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
 
-  const answer = await answerQuestion.execute({
-    questionId: '1',
-    instructorId: '1',
-    content: 'new answer'
+    SUT = new AnswerQuestionUseCase(inMemoryAnswersRepository, inMemoryQuestionsRepository)
   })
 
-  expect(answer.content).toEqual('new answer')
+  it('should be able to create an answer to a question', async () => {
+    const newQuestion = makeQuestion()
+
+    const question = await inMemoryQuestionsRepository.create(newQuestion)
+
+    const { answer } = await SUT.execute({
+      questionId: question.id.toString(),
+      authorId: 'author',
+      content: 'new answer'
+    })
+
+    expect(inMemoryAnswersRepository.answers).toHaveLength(1)
+    expect(inMemoryAnswersRepository.answers[0]).toHaveProperty('id')
+    expect(inMemoryAnswersRepository.answers[0].id.toString()).toEqual(answer.id.toString())
+    expect(inMemoryAnswersRepository.answers[0]).toHaveProperty('authorId')
+    expect(inMemoryAnswersRepository.answers[0].authorId.toString()).toEqual(answer.authorId.toString())
+    expect(inMemoryAnswersRepository.answers[0]).toHaveProperty('questionId')
+    expect(inMemoryAnswersRepository.answers[0].questionId.toString()).toEqual(answer.questionId.toString())
+    expect(inMemoryAnswersRepository.answers[0]).toHaveProperty('content')
+    expect(inMemoryAnswersRepository.answers[0].content).toEqual(answer.content)
+  })
+
+  it('should throw an error when the question does not found', async () =>
+    expect(async () => await SUT.execute({ questionId: faker.lorem.text(), authorId: '1', content: 'new answer' }))
+      .rejects.toThrowError('Pergunta não encontrada.')
+  )
 })
+
--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -1,25 +1,32 @@
 import { Answer } from '../../enterprise/entities/answer'
 import { AnswersRepository } from '../repositories/answers-repository'
+import { QuestionsRepository } from '../repositories/questions-repository'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 interface IAnswerQuestionUseCaseParams {
-  instructorId: string
+  authorId: string
   questionId: string
   content: string
 }
 
 export class AnswerQuestionUseCase {
-  constructor(private answersRepository: AnswersRepository) { }
+  constructor(
+    private answersRepository: AnswersRepository,
+    private questionsRepository: QuestionsRepository
+  ) { }
 
-  async execute({ content, questionId, instructorId }: IAnswerQuestionUseCaseParams) {
+  async execute({ content, questionId, authorId }: IAnswerQuestionUseCaseParams) {
+    const questionFound = await this.questionsRepository.findById(questionId)
+    if (!questionFound) throw new Error('Pergunta não encontrada.')
+
     const answer = Answer.create({
       content,
       questionId: new UniqueEntityID(questionId),
-      authorId: new UniqueEntityID(instructorId),
+      authorId: new UniqueEntityID(authorId),
     })
 
     await this.answersRepository.create(answer)
 
-    return answer
+    return { answer }
   }
 }
